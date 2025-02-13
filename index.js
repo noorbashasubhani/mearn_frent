6be30be33b99b56076060c6b6fa3d9154e038444
noorbasha <nbs786786@gmail.com>
2025-02-13 17:14:01 +0530

    push code

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const vendorRoutes=require('./routes/vendorRoutes');
 const bodyParser=require('body-parser');
+const cors = require('cors');
+
 
 const app = express();
 const PORT =  process.env.PORT || 4000;
 
+
+app.use(cors());
 dotenv.config();  // Load environment variables from .env
 // Log the MONGO_URL to verify it is being loaded
 console.log("MongoDB URL:", process.env.MONGO_URL);
