9fa4a378849f54e515c45336fbd3829ca56def88
noorbasha <nbs786786@gmail.com>
2025-02-19 14:32:53 +0530

    Add login user

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -12,7 +12,7 @@ const express = require('express');
 const router=express.Router();
 
 router.post('/register',vendorController.vendorRegister);
-router.post('/login',vendorController.vendorLogin);
+//router.post('/User-Login',vendorController.vendorLogin);
 router.post("/Add-Dept", deparmentController.addDepartment);
 router.get("/Dept",deparmentController.getDepartments);
 router.post("/Add-Designations",designationController.addDesignation);
